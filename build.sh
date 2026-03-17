4f41b9fd3d79b409037f247262186a7028178ef2
root <root@DESKTOP-LF2IERR.localdomain>
2026-03-17 08:00:01 +0100

    Corregir build.sh con formato Unix y permisos

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-echo "Compilando dylib con proteccion..."
+echo "Compilando dylib para iOS..."
+
+# Buscar el SDK de iOS más reciente
+IOS_SDK=$(xcrun --sdk iphoneos --show-sdk-path)
+
 clang -dynamiclib \
+  -arch arm64 \
+  -isysroot $IOS_SDK \
+  -miphoneos-version-min=9.0 \
   -fvisibility=hidden \
   -fblocks \
   -framework Foundation \
@@ -14,4 +21,3 @@ clang -dynamiclib \
   FIXToast.m \
   APIClient_Twink.m \
   Constructor.m
-EOF
